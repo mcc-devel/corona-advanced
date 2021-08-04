67636351c142e7577d314beb2dc9b9324b9b878f
mcc-devel <jettzheng@foxmail.com>
2021-08-04 16:40:14 +0800

    update files and add recoveries

--- corona-advanced.py
+++ corona-advanced.py
@@ -9,27 +9,38 @@ from pyecharts.globals import ThemeType
 
 confirm = requests.get('https://raw.fastgit.org/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
 death = requests.get('https://raw.fastgit.org/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv')
-os.system('rm -rf confirm.csv death.csv confirm.json death.json')
+recover = requests.get('https://raw.fastgit.org/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv')
+os.system('rm -rf confirm.csv death.csv recover.csv confirm.json death.json recover.json')
 fc = open('confirm.csv', 'x', encoding = 'utf-8')
 fd = open('death.csv', 'x', encoding = 'utf-8')
+fr = open('recover.csv', 'x', encoding = 'utf-8')
 fc.write(confirm.text)
 fd.write(death.text)
+fr.write(recover.text)
 fc.close()
 fd.close()
+fr.close()
 jsnc = []
 jsnd = []
+jsnr = []
 fjsnc = open('confirm.json', 'x', encoding = 'utf-8')
 fjsnd = open('death.json', 'x', encoding = 'utf-8')
+fjsnr = open('recover.json', 'x', encoding = 'utf-8')
 fc = open('confirm.csv', 'r', encoding = 'utf-8')
 fd = open('death.csv', 'r', encoding = 'utf-8')
+fr = open('recover.csv', 'r', encoding = 'utf-8')
 rdc = DictReader(fc)
 rdd = DictReader(fd)
+rdr = DictReader(fr)
 
 for row in rdc:
     jsnc.append(row)
 
 for row in rdd:
     jsnd.append(row)
+
+for row in rdr:
+    jsnr.append(row)
 
 json.dump(jsnc, fjsnc)
 json.dump(jsnd, fjsnd)
@@ -38,6 +49,7 @@ fjsnd.close()
 print('Information is stored in json format if needed')
 dead = {}
 conf = {}
+rcov = {}
 dates = []
 delta = timedelta(1)
 start = date(2020, 1, 22)
@@ -53,6 +65,7 @@ countries = []
 for dat in dates:
     conf[dat] = {}
     dead[dat] = {}
+    rcov[dat] = {}
     for elem in jsnc:
         if elem['Country/Region'] == 'Summer Olympics 2020':
             continue
@@ -143,6 +156,51 @@ for dat in dates:
                 dead[dat][country] = {}
                 dead[dat][country]['children'] = []
             dead[dat][country]['children'].append({elem['Province/State'] : elem[dat]})
+    for elem in jsnr:
+        if elem['Country/Region'] == 'Summer Olympics 2020':
+            continue
+        if elem['Country/Region'] == 'US':
+            elem['Country/Region'] = 'United States'
+        if elem['Country/Region'] == 'Korea, South':
+            elem['Country/Region'] = 'Dem. Rep. Korea'
+        if elem['Country/Region'] == 'Congo (Brazzaville)':
+            elem['Country/Region'] = 'Congo'
+        if elem['Country/Region'] == 'Congo (Kinshasa)':
+            elem['Country/Region'] = 'Dem. Rep. Congo'
+        if elem['Country/Region'] == 'South Sudan':
+            elem['Country/Region'] = 'S. Sudan'
+        if elem['Country/Region'] == 'Central African Republic':
+            elem['Country/Region'] = 'Central African Rep.'
+        if elem['Province/State'] == 'Greenland':
+            elem['Country/Region'] = 'Greenland'
+            elem['Province/State'] = ''
+        if elem['Country/Region'] not in countries:
+            countries.append(elem['Country/Region'])
+        if elem['Province/State'] == '':
+            if elem['Country/Region'] not in rcov[dat].keys():
+                rcov[dat][elem['Country/Region']] = {}
+            rcov[dat][elem['Country/Region']]['total'] = elem[dat]
+            if 'children' not in rcov[dat][elem['Country/Region']].keys():
+                rcov[dat][elem['Country/Region']]['children'] = None
+            if flag == True:
+                flag = False
+                rcov[dat][country]['total'] = total
+                total = 0
+                country = ''
+        else:
+            if elem['Country/Region'] != country and country != '':
+                flag = False
+                rcov[dat][country]['total'] = total
+                total = 0
+                country = ''
+            if flag == True:
+                total = total + int(elem[dat])
+            if flag == False:
+                flag = True
+                country = elem['Country/Region']
+                rcov[dat][country] = {}
+                rcov[dat][country]['children'] = []
+            rcov[dat][country]['children'].append({elem['Province/State'] : elem[dat]})
 
 timeline = (
     Timeline(init_opts=opts.InitOpts(page_title='Coronavirus', theme=ThemeType.LIGHT))
@@ -150,13 +208,18 @@ timeline = (
 timeline2 = (
     Timeline(init_opts=opts.InitOpts(page_title='Coronavirus', theme=ThemeType.LIGHT))
 )
+timeline3 = (
+    Timeline(init_opts=opts.InitOpts(page_title='Coronavirus', theme=ThemeType.LIGHT))
+)
 for i in range(len(dates)):
     dat = dates[i]
     czipped = []
     dzipped = []
+    rzipped = []
     for cntry in countries:
         czipped.append((cntry, conf[dat][cntry]['total']))
         dzipped.append((cntry, dead[dat][cntry]['total']))
+        rzipped.append((cntry, rcov[dat][cntry]['total']))
     map = (
         Map()
         .add('Confirmed', czipped, 'world')
@@ -196,11 +259,35 @@ for i in range(len(dates)):
             {'min':0, 'max':19, 'color':'#feeeed'}
         ]))
     )
+    map3 = (
+        Map()
+        .add('Recovered', rzipped, 'world')
+        .set_series_opts(label_opts=opts.LabelOpts(position='right'))
+        .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
+        .set_global_opts(visualmap_opts=opts.VisualMapOpts(is_piecewise=True, pieces=[
+            {'min':10000000, 'label':'10000000+', 'color':'#009ad6'},
+            {'min':1000000, 'max':9999999, 'color':'#00ae9d'},
+            {'min':500000, 'max':999999, 'color':'#78a355'},
+            {'min':100000, 'max':499999, 'color':'#5c7a29'},
+            {'min':50000, 'max':99999, 'color':'#a3cf62'},
+            {'min':6000, 'max':49999, 'color':'#b2d235'},
+            {'min':2000, 'max':5999, 'color':'#7fb80e'},
+            {'min':300, 'max':1999, 'color':'#007d65'},
+            {'min':150, 'max':299, 'color':'#007947'},
+            {'min':90, 'max':149, 'color':'#1d953f'},
+            {'min':40, 'max':89, 'color':'#84bf96'},
+            {'min':20, 'max':39, 'color':'#abc88b'},
+            {'min':0, 'max':19, 'color':'#cde6c7'}
+        ]))
+    )
     timeline.add(map, dat)
     timeline2.add(map2, dat)
-timeline.add_schema(play_interval=30)
+    timeline3.add(map3, dat)
+timeline.add_schema(play_interval=50)
 timeline.render('Corona-confirms.html')
-timeline2.add_schema(play_interval=30)
+timeline2.add_schema(play_interval=50)
 timeline2.render('Corona-deaths.html')
+timeline3.add_schema(play_interval=50)
+timeline3.render('Corona-recoveries.html')
 print('Generated graphs')
 print('Done')
